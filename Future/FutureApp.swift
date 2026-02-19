06b741935b674973b97a36f441c61ffb00a40cd8
Faisal Sayed <faisal.sayed502@gmail.com>
2026-02-19 00:29:12 -0800

    Initial Commit

---
+++ Future/FutureApp.swift
@@ -0,0 +1,17 @@
+//
+//  FutureApp.swift
+//  Future
+//
+//  Created by Faisal Sayed on 2/19/26.
+//
+
+import SwiftUI
+
+@main
+struct FutureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
